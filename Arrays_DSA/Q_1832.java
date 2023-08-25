15e770af5055e2fc58ef04589610df19342a149f
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-08-25 23:58:50 +0530

    Update Q_1832.java

--- Arrays_DSA/Q_1832.java
+++ Arrays_DSA/Q_1832.java
@@ -2,7 +2,6 @@
 //A pangram is a sentence where every letter of the English alphabet appears at least once.
 //Given a string sentence containing only lowercase English letters, return true if sentence is a pangram, or false otherwise.
 import java.util.Arrays;
-//ASCII for a to z : 97 to 122
 public class Q_1832 {
     public static void main(String[] args) {
         String sentence = "uwqohxamknblecdtzpisgvyfjr";
@@ -15,7 +14,7 @@ public class Q_1832 {
             return false;
         }
         for (int i = 0; i < sentence.length(); i++) {
-                int j=sentence.charAt(i)-97;
+                int j=sentence.charAt(i)-97;  //ASCII for a to z : 97 to 122
                 arr[j]=1;
         }
         System.out.println(Arrays.toString(arr));
