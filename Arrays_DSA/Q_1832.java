d3c488bb549d70cf2df9b6b2089e0b11d423f649
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-08-25 23:57:45 +0530

    Add files via upload

---
+++ Arrays_DSA/Q_1832.java
@@ -0,0 +1,36 @@
+//Leetcode Q 1832. Check if the Sentence Is Pangram
+//A pangram is a sentence where every letter of the English alphabet appears at least once.
+//Given a string sentence containing only lowercase English letters, return true if sentence is a pangram, or false otherwise.
+import java.util.Arrays;
+//ASCII for a to z : 97 to 122
+public class Q_1832 {
+    public static void main(String[] args) {
+        String sentence = "uwqohxamknblecdtzpisgvyfjr";
+        checkIfPangram(sentence);
+    }
+    static boolean checkIfPangram(String sentence) {
+        boolean ans=false;
+        int[] arr= new int[26];
+        if(sentence.length()<26){
+            return false;
+        }
+        for (int i = 0; i < sentence.length(); i++) {
+                int j=sentence.charAt(i)-97;
+                arr[j]=1;
+        }
+        System.out.println(Arrays.toString(arr));
+        for (int i = 0; i <arr.length ; i++) {
+            if (arr[i]!=1){
+                ans=false;
+                break;
+            }else {
+                ans=true;
+            }
+        }
+        System.out.println(ans);
+        System.out.println(sentence);
+        System.out.println(sentence.length());
+
+        return ans;
+    }
+}
