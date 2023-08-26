56fab0cfd7398e267dd5befd7db5695b827d8f7c
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-08-26 22:21:51 +0530

    Add files via upload

---
+++ Arrays_DSA/Q_1512.java
@@ -0,0 +1,28 @@
+//LeetCode Q 1512 : Number of Good Pairs
+//Given an array of integers nums, return the number of good pairs.
+//A pair (i, j) is called good if nums[i] == nums[j] and i < j.
+//Input: nums = [1,2,3,1,1,3]
+//Output: 4
+//Explanation: There are 4 good pairs (0,3), (0,4), (3,4), (2,5) 0-indexed.
+
+public class Q_1512 {
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,1,1,3};
+
+        numIdenticalPairs(nums);
+    }
+    static int numIdenticalPairs(int[] nums) {
+        int count=0;
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i+1; j < nums.length; j++) {
+//                System.out.println(i+" "+j);
+                if (nums[i] == nums[j]){
+                    count++;
+                }
+            }
+        }
+        System.out.println("Good Pairs :"+count);
+        return count;
+    }
+}
