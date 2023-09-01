f5a5e33c60806d543334ba9968f23279766e1c4b
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-09-01 23:23:53 +0530

    Create Q_0832.java

---
+++ Arrays_DSA/Q_0832.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+public class Q_832 {
+    public static void main(String[] args) {
+       int image[][]={{1, 1, 0}, {1, 0, 1}, {0, 0, 0}};
+
+       flipAndInvertImage(image);
+    }
+  
+    static int[][] flipAndInvertImage(int[][] image) {
+        int [][] ans = new int[image.length][];
+        for (int i = 0; i < image.length; i++) {
+            ans[i]=new int[image[i].length];
+            int k = image[i].length-1;  //k pointer for reversal
+            for (int j = 0 ; j< image[i].length ; j++) {
+                ans[i][j]=flip(image[i][k]);
+                k--;
+            }
+        }
+
+        System.out.println(Arrays.deepToString(ans));
+        return ans;
+    }
+
+    static int flip(int a){  //flips the bit
+        if (a==0){
+            return 1;
+        }else{
+            return 0;
+        }
+    }
+}
