2303f740e18b2c13cb84513217179641ed1fceed
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-09-02 12:43:56 +0530

    Update Q_1572.java

--- Arrays_DSA/Q_1572.java
+++ Arrays_DSA/Q_1572.java
@@ -6,15 +6,24 @@ public class Q_1572 {
     }
 
     static int diagonalSum(int[][] mat) {
-        int ans,pri_sum;
-        ans = pri_sum = 0;
+        int ans,pri_sum,sec_sum,mid_val;
+        ans = pri_sum = sec_sum =mid_val= 0;
         int j =mat.length-1;
 
-       //  primary sum
-       for (int i = 0; i < mat.length; i++) {
-           pri_sum+=mat[i][i];
-       }
-       System.out.println("pri_sum :"+pri_sum);
+        for (int i = 0; i < mat.length; i++) {
+            if (i==j){
+               mid_val=mat[i][j];
+            }
+            pri_sum+=mat[i][i];
+            sec_sum+=mat[i][j];
+            j--;
+        }
+        System.out.println("pri_sum :"+pri_sum);
+        System.out.println("sec_sum :"+sec_sum);
+        System.out.println("mid_val :"+mid_val);
+
+        ans=pri_sum+sec_sum-mid_val;
+        System.out.println("ans :"+ans);
 
         return ans;
     }
