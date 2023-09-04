b87bae1099dbb7d87767bb6b621dc9e24eb38f6a
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-09-04 23:40:10 +0530

    Create Q_1295.java

---
+++ Arrays_DSA/Q_1295.java
@@ -0,0 +1,16 @@
+public class Q_1295 {
+    public static void main(String[] args) {
+        int[] nums = new int[ ] {12, 345, 2, 6, 7896};
+        findNumbers(nums);
+    }
+
+
+  
+    static int findNumbers(int[] nums) {
+        int ans=0;
+
+
+        return ans;
+    }
+
+}
