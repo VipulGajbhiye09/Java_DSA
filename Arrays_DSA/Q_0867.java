6ebc0d2184bd1cf447e2d7f080817452e3c15798
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-09-06 23:01:18 +0530

    Update Q_0867.java

--- Arrays_DSA/Q_0867.java
+++ Arrays_DSA/Q_0867.java
@@ -1,3 +1,13 @@
+//LeetCode Q867 : Transpose matrix
+//Given a 2D integer array matrix, return the transpose of matrix.
+//The transpose of a matrix is the matrix flipped over its main diagonal, switching the matrix's row and column indices.
+//Input: matrix = [[1,2,3],
+//                 [4,5,6],
+//                 [7,8,9]]
+//Output:         [[1,4,7],
+//                 [2,5,8],
+//                 [3,6,9]]
+
 import java.util.Arrays;
 
 public class Q_867 {
