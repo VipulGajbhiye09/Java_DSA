c32d61e28a072dfc463476f912ad1150e72d3fec
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-09-07 23:07:13 +0530

    Update Q_1854.java

--- Arrays_DSA/Q_1854.java
+++ Arrays_DSA/Q_1854.java
@@ -1,3 +1,10 @@
+//1854. Maximum Population Year
+//given a 2D integer array logs where each logs[i] = [birth(i), death(i)] indicates the birth and death years of the ith person.
+//The population of some year x is the number of people alive during that year.
+// The ith person is counted in year x's population if x is in the inclusive range [birthi, deathi - 1]. 
+// Note that the person is not counted in the year that they die.
+//Return the earliest year with the maximum population.
+
 public class Q_1854 {
     public static void main(String[] args) {
         int[][] logs = {{1993, 1999}, {2000, 2010}};
