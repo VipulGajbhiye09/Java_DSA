0b0a18e4044b6674a67f59ad88cbf08a6e32c244
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-09-08 23:13:43 +0530

    Update Q_0001.java

--- Arrays_DSA/Q_0001.java
+++ Arrays_DSA/Q_0001.java
@@ -1,3 +1,6 @@
+// Q1 Two sum
+//Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+//You may assume that each input would have exactly one solution, and you may not use the same element twice.
 import java.util.Arrays;
 
 public class Q_1 {
