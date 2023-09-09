26e843e2ce5424fe86fbf89f121a70cda139ab4d
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-09-09 23:03:49 +0530

    Create Q_1217.java

---
+++ Arrays_DSA/Q_1217.java
@@ -0,0 +1,13 @@
+public class Q_1217 {
+    public static void main(String[] args) {
+        int [] position = {2, 2, 2, 3, 3};
+        minCostToMoveChips(position);
+    }
+
+    static int minCostToMoveChips(int[] position) {
+        int cost=0;
+
+
+        return cost;
+    }
+}
