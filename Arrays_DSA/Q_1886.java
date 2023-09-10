018cec4b6c5731259f1252936648d4207cc852e7
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-09-10 19:55:50 +0530

    Update Q_1886.java

--- Arrays_DSA/Q_1886.java
+++ Arrays_DSA/Q_1886.java
@@ -1,1 +1,15 @@
+public class Q_1886 {
+    public static void main(String[] args) {
+        int[][] mat = {{0, 0, 0}, {0, 1, 0}, {1, 1, 1}};
+        int[][] target = {{1, 1, 1}, {0, 1, 0}, {0, 0, 0}};
+        findRotation(mat,target);
+    }
 
+
+    static boolean findRotation(int[][] mat, int[][] target) {
+        boolean ans=true;
+
+        
+        return ans;
+    }
+}
