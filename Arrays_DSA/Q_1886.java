00387a460c62c644d76d430ad987392965f5343d
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-09-10 19:57:44 +0530

    Update Q_1886.java

--- Arrays_DSA/Q_1886.java
+++ Arrays_DSA/Q_1886.java
@@ -1,3 +1,9 @@
+//Leetcode 1886. Determine Whether Matrix Can Be Obtained By Rotation
+//Given two n x n binary matrices mat and target, return true if it is possible to make mat equal to target by rotating 
+//matrix in 90-degree increments, or false otherwise.
+//(1) Transpose matrix
+//(2) Flip matrix along vertical symmetry line
+
 public class Q_1886 {
     public static void main(String[] args) {
         int[][] mat = {{0, 0, 0}, {0, 1, 0}, {1, 1, 1}};
