868c9ad3c0de908e2244927a7d6602e445d7dc6a
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-09-10 19:58:15 +0530

    Update Q_1886.java

--- Arrays_DSA/Q_1886.java
+++ Arrays_DSA/Q_1886.java
@@ -18,4 +18,22 @@ public class Q_1886 {
         
         return ans;
     }
+    
+    static int[][] transpose(int[][] matrix) {
+            int[][] ans=new int[matrix[0].length][matrix.length];
+            for (int i = 0; i < matrix[0].length; i++) {
+                for (int j = 0; j < matrix.length; j++) {
+                    ans[i][j]=matrix[j][i];
+                }
+            }
+    //      System.out.println(Arrays.deepToString(ans));
+            return ans;
+        }
+
+
+
+
+
+
+
 }
