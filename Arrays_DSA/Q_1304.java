3daa435a9172ea0423af53ef55ceb85bae0ecd38
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-09-12 17:57:12 +0530

    Update Q_1304.java

--- Arrays_DSA/Q_1304.java
+++ Arrays_DSA/Q_1304.java
@@ -1,11 +1,25 @@
+import java.util.Arrays;
+
 public class Q_1304 {
     public static void main(String[] args) {
-
+        sumZero(9);
     }
 
     static int[] sumZero(int n) {
         int[] ans=new int[n];
-
+        if (n%2!=0){
+            ans[0]=0;
+            for (int i = 1; i < ans.length; i+=2) {
+                ans[i]=i;
+                ans[i+1]=-i;
+            }
+        }else {
+            for (int i = 0; i < ans.length; i+=2) {
+                ans[i]=i+1;
+                ans[i+1]=-(i+1);
+            }
+        }
+        System.out.println(Arrays.toString(ans));
         return ans;
     }
 }
