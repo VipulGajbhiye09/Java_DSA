779b28320d73a7b10217268e025d815aecbcf058
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-09-13 23:13:48 +0530

    Create Q_1380.java

---
+++ Arrays_DSA/Q_1380.java
@@ -0,0 +1,17 @@
+//Leetcode 1380. Lucky Numbers in a Matrix
+//Given an m x n matrix of distinct numbers, return all lucky numbers in the matrix in any order.
+//A lucky number is an element of the matrix such that it is the minimum element in its row and maximum in its column.
+
+public class Q_1380 {
+    public static void main(String[] args) {
+        int[][] matrix = {{3,7,8}, {9,11,13}, {15,16,17}};
+        luckyNumbers(matrix);
+    }
+
+  static ArrayList<Integer> luckyNumbers (int[][] matrix) {
+        ArrayList<Integer> ans = new ArrayList<>(2);
+
+
+        return ans;
+    }
+}
