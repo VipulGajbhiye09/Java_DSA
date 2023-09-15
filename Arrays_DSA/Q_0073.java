0db061736a5160dc5933763450fd8ade1ed258f0
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-09-15 22:24:10 +0530

    Create Q_0073.java

---
+++ Arrays_DSA/Q_0073.java
@@ -0,0 +1,10 @@
+public class Q_0073 {
+    public static void main(String[] args) {
+        int[][] matrix = {{3,7,8}, {9,11,13}, {15,16,17}};
+        setZeroes(matrix);
+    }
+
+    static void setZeroes(int[][] matrix) {
+
+    }
+}
