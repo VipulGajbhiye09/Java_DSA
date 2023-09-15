e59495195cdc87b9bda5c54c09e30702ab87ce8e
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-09-15 22:23:52 +0530

    Create Q_0054.java

---
+++ Arrays_DSA/Q_0054.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Q_0054 {
+    public static void main(String[] args) {
+        int[][] matrix = {{3,7,8}, {9,11,13}, {15,16,17}};
+        spiralOrder(matrix);
+    }
+
+    public static ArrayList<Integer> spiralOrder(int[][] matrix) {
+        ArrayList<Integer> ans = new ArrayList<>(matrix.length);
+        int count=0;
+        int ele_count=matrix.length*matrix[0].length;
+
+
+
+        return ans;
+    }
+
+}
