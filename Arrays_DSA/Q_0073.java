96e4fa7ae685c6ced182bd6c79c8b606838e4d9d
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-09-16 22:38:30 +0530

    Update Q_0073.java

--- Arrays_DSA/Q_0073.java
+++ Arrays_DSA/Q_0073.java
@@ -1,3 +1,7 @@
+//Leetcode Medium 73.Set Matrix Zeroes
+//Given an m x n integer matrix, if an element is 0, set its entire row and column to 0's.
+//You must do it using "in place" algo.
+
 public class Q_0073 {
     public static void main(String[] args) {
         int[][] matrix = {{3,7,8}, {9,11,13}, {15,16,17}};
@@ -6,5 +10,16 @@ public class Q_0073 {
 
     static void setZeroes(int[][] matrix) {
 
+    }
+    static void setColZero(int[][] matrix,int col){
+        for (int i = 0; i < matrix.length; i++) {
+            matrix[i][col]=0;
+        }
+    }
+
+    static void setRowZero(int[][] matrix,int row){
+        for (int i = 0; i < matrix[0].length; i++) {
+            matrix[row][i]=0;
+        }
     }
 }
