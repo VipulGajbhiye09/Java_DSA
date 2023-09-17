da8f2d2c37598e465dd5e5b43fa5907adce94904
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-09-17 21:54:28 +0530

    Update Q_0054.java

--- Arrays_DSA/Q_0054.java
+++ Arrays_DSA/Q_0054.java
@@ -13,9 +13,35 @@ public class Q_0054 {
         top=left=0;
         right=matrix[0].length-1;
         bottom=matrix.length-1;
+        
+        while (top<=bottom && left<=right){
+            
+            for (int i = left; i <=right; i++) {
+                ans.add(matrix[top][i]);
+            }
+            top++;
+            for (int i = top; i <=bottom ; i++) {
+                ans.add(matrix[i][right]);
+            }
+            right--;
+            if (top<=bottom){
+                for (int i = right; i>=left; i--) {
+                    ans.add(matrix[bottom][i]);
+                }
+            }
+            bottom--;
+            if (left<=right){
+                for (int i = bottom; i>=top; i--) {
+                    ans.add(matrix[i][left]);
+                }
+                left++;
+            }
+            
+        }
 
         System.out.println(ans);
         return ans;
-    }
 
+        
+    }
 }
