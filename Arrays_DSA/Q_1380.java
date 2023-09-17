63553a0c51bb3a608c84326020663be04b478092
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-09-17 21:51:35 +0530

    Update Q_1380.java

--- Arrays_DSA/Q_1380.java
+++ Arrays_DSA/Q_1380.java
@@ -8,19 +8,39 @@ public class Q_1380 {
         luckyNumbers(matrix);
     }
 
-  static ArrayList<Integer> luckyNumbers (int[][] matrix) {
-        ArrayList<Integer> ans = new ArrayList<>(2);
+  public static ArrayList<Integer> luckyNumbers(int[][] matrix) {
+        ArrayList<Integer> ans = new ArrayList<>();
 
+        for (int i = 0; i < matrix.length; i++) {
+            int minRow = minOfRow(matrix[i]);
+            int col = indexOfMin(matrix[i]);
 
+            if (minRow == maxOfCol(matrix, col)) {
+                ans.add(minRow);
+            }
+        }
+
+        System.out.println(ans);
         return ans;
     }
-    
+
     static int minOfRow(int[] arr){
         Arrays.sort(arr);
         int ans=arr[0];
         return ans;
     }
-    
+    static int indexOfMin(int[] arr) {
+        int min = Integer.MAX_VALUE;
+        int index = -1;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+                index = i;
+            }
+        }
+        return index;
+    }
     static int maxOfCol(int[][] mat,int col){
         int [] arr = new int[mat.length];
         for (int i = 0; i < mat.length; i++) {
