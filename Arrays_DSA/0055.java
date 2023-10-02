8ae7261493b0436f5034c9e88c58ee1c2094ba92
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-10-02 23:30:28 +0530

    Create 0055.java

---
+++ Arrays_DSA/0055.java
@@ -0,0 +1,18 @@
+//Leetcode Medium 55. Jump-Game
+//You are given an integer array nums. You are initially positioned at the array's first index,
+//and each element in the array represents your maximum jump length at that position.
+//Return true if you can reach the last index, or false otherwise.
+//Input: nums = [2,3,1,1,4]
+//Output: true
+//Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+
+public class Q_0055 {
+    public static void main(String[] args) {
+        int[] nums = {2, 3, 1, 0, 4};
+        System.out.println(canJump(nums));
+    }
+
+    static boolean canJump(int[] nums) {
+
+    }
+}
