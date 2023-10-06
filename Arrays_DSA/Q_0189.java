f5208eda8229d5488f50a18cc78e1295c4fff2f5
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-10-06 22:33:13 +0530

    Create Q_0189.java

---
+++ Arrays_DSA/Q_0189.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class Q_0189 {
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4,5,6,7};
+        int k = 3;
+        rotate(nums, k);
+    }
+
+    static void rotate(int[] nums, int k) {
+
+    }
+
+}
+
