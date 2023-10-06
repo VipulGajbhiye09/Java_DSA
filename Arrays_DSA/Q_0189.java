bd5426efbdfbd377e4de1527b5e4849808b68a0c
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-10-06 22:33:25 +0530

    Update Q_0189.java

--- Arrays_DSA/Q_0189.java
+++ Arrays_DSA/Q_0189.java
@@ -8,6 +8,19 @@ public class Q_0189 {
     }
 
     static void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k = k % n;
+        int[] result = new int[n];
+
+        if (k == 0) {
+            return;
+        }
+        for (int i = 0; i < n; i++) {
+            result[(i + k) % n] = nums[i];
+        }
+
+        System.arraycopy(result, 0, nums, 0, n);
+        System.out.println(Arrays.toString(nums));
 
     }
 
