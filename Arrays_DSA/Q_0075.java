9ee1f9e595811548386f21cca1abc0e5ca8bebbc
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-10-07 23:02:16 +0530

    Update Q_0075.java

--- Arrays_DSA/Q_0075.java
+++ Arrays_DSA/Q_0075.java
@@ -6,6 +6,17 @@ public class Q_0075 {
         sortColors(nums);
     }
     static void sortColors(int[] nums) {
+        int c0,c1,c2;
+        c0=c1=c2=0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i]==0){
+                c0++;
+            }else if (nums[i]==1) {
+                c1++;
+            }else {
+               c2++;
+            }
+        }
 
     }
 }
