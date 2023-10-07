8289cd7b07095d3aa204a244c4f02e62fed897cb
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-10-07 23:01:58 +0530

    Create Q_0075.java

---
+++ Arrays_DSA/Q_0075.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class Q_0075 {
+    public static void main(String[] args) {
+        int[] nums = {2, 0, 2, 1, 1, 0};
+        sortColors(nums);
+    }
+    static void sortColors(int[] nums) {
+
+    }
+}
